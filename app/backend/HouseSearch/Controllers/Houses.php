eba2ac13214b7aae5b94a74d8e796144f724fc80
Laus Brandt <laus@equites.dk>
2020-11-21 10:12:32 +0100

    Initial commit

---
+++ app/backend/HouseSearch/Controllers/Houses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HouseSearch\Controllers;
+
+use Core\Controller\Json;
+
+class Houses extends Json
+{
+    public function GET()
+    {
+        return 'hey';
+    }
+}
---
+++ app/public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include __DIR__ . "/../../vendor/autoload.php";
+
+define('APP_ROOT', __DIR__ . "/..");
+
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, content-type, Authorization');
+
+$router = new \Core\Router();
+$router->loadRoutes(APP_ROOT . "/backend/config/structure.xml");
+$router->setNameSpace("HouseSearch\Controllers");
+$router->run();
